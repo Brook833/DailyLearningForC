7eb331e682cc4e4c6a3e8013ff1f6f034cfb308e
Brook833 <964785916@qq.com>
2022-10-21 23:06:13 +0800

    day36 work

--- day35/person.h
+++ day35/person.h
@@ -10,6 +10,16 @@ class Person {
  public:
   string getName() const { return strName_; }      //返回姓名
   string getAdress() const { return strAdress_; }  //返回地址
+  istream &read(istream &is, Person &per);
+  ostream &print(ostream &os, const Person &per);
 };
 
+istream & Person::read(istream &is, Person &per) {
+  is >> per.strName_ >> per.strAdress_;
+  return is;
+}
+
+ostream & Person::print(ostream &os, const Person &per) {
+  os << per.getName() << per.getAdress();
+}
 #endif
